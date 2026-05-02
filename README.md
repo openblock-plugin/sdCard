62e018c02782d8f21ca721607022646e0a17e9a0
Arthur Zheng <arthurzheng150@gmail.com>
2026-05-02 13:21:53 +0800

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# SD Card Module
+
+![Version](https://img.shields.io/github/v/tag/openblock-plugin/sdCard?label=version)
+![License](https://img.shields.io/badge/license-MIT-green.svg)
+![Type](https://img.shields.io/badge/type-extension-orange.svg)
+![Tags](https://img.shields.io/badge/tags-other-lightgrey.svg)
+
+Save or read your data in SD card.
+
+## Documentation
+
+For detailed usage instructions, please visit:
+
+**[OpenBlock Wiki](https://wiki.openblock.cc)**
---
+++ src/translations.js
@@ -0,0 +1,115 @@
+/* eslint-disable quote-props */
+/* eslint-disable max-len */
+/**
+ * Translation file for this resouce.
+ *
+ * IMPORTANT:
+ * - The "en" (English) section is automatically generated from source files.
+ *   Do NOT modify the "en" section manually.
+ * - Other language sections (e.g., "zh-cn", "zh-tw", "ja", etc.) should be
+ *   manually translated by you.
+ * - When you run the extraction script again, only the "en" section will be
+ *   updated. Your manual translations in other languages will be preserved.
+ *
+ * Structure:
+ * - interface: translations for name/description (used by GUI formatMessage)
+ * - extensions: translations for extension blocks (used by VM formatMessage)
+ * - blocks: translations for Blockly blocks (used by Blockly.Msg)
+ */
+
+export default {
+    'interface': {
+        'en': {
+            'sdCard.name': 'SD Card Module',
+            'sdCard.description': 'Save or read your data in SD card.'
+        },
+        'ru': {
+            'sdCard.name': 'SD Card Module',
+            'sdCard.description': 'Save or read your data in SD card.'
+        },
+        'zh-cn': {
+            'sdCard.name': 'SD Card Module',
+            'sdCard.description': 'Save or read your data in SD card.'
+        },
+        'zh-tw': {
+            'sdCard.name': 'SD Card Module',
+            'sdCard.description': 'Save or read your data in SD card.'
+        }
+    },
+    'extensions': {
+        'en': {},
+        'ru': {},
+        'zh-cn': {},
+        'zh-tw': {}
+    },
+    'blocks': {
+        'en': {
+            'SDCARD_CATEGORY': 'SD Card',
+            'SDCARD_INIT': 'init sd card pin CS %1',
+            'SDCARD_OPENFILE': 'sd card open file %1 mode %2',
+            'SDCARD_MODE_READ': 'read only',
+            'SDCARD_MODE_READWRITE': 'read & write',
+            'SDCARD_OPENFILE_TOOLTIP': 'If file not exists, it will be created automatically.',
+            'SDCARD_CLOSEFILE': 'sd card close file',
+            'SDCARD_PRINT': 'print %1 %2 to file',
+            'SDCARD_EOL_WARP': 'warp',
+            'SDCARD_EOL_NOWARP': 'no-warp',
+            'SDCARD_FILEDATAAVAILABLE': 'sd card file data available',
+            'SDCARD_READFILEDATA': 'sd card read file data',
+            'SDCARD_ISFILEEXISTS': 'sd card file %1 exists?',
+            'SDCARD_CREATEFILE': 'sd card create file %1',
+            'SDCARD_DELETEFILE': 'sd card delet file %1'
+        },
+        'ru': {
+            'SDCARD_CATEGORY': 'SD Card',
+            'SDCARD_INIT': 'init sd card pin CS %1',
+            'SDCARD_OPENFILE': 'sd card open file %1 mode %2',
+            'SDCARD_MODE_READ': 'read only',
+            'SDCARD_MODE_READWRITE': 'read & write',
+            'SDCARD_OPENFILE_TOOLTIP': 'If file not exists, it will be created automatically.',
+            'SDCARD_CLOSEFILE': 'sd card close file',
+            'SDCARD_PRINT': 'print %1 %2 to file',
+            'SDCARD_EOL_WARP': 'warp',
+            'SDCARD_EOL_NOWARP': 'no-warp',
+            'SDCARD_FILEDATAAVAILABLE': 'sd card file data available',
+            'SDCARD_READFILEDATA': 'sd card read file data',
+            'SDCARD_ISFILEEXISTS': 'sd card file %1 exists?',
+            'SDCARD_CREATEFILE': 'sd card create file %1',
+            'SDCARD_DELETEFILE': 'sd card delet file %1'
+        },
+        'zh-cn': {
+            'SDCARD_CATEGORY': 'SD Card',
+            'SDCARD_INIT': 'init sd card pin CS %1',
+            'SDCARD_OPENFILE': 'sd card open file %1 mode %2',
+            'SDCARD_MODE_READ': 'read only',
+            'SDCARD_MODE_READWRITE': 'read & write',
+            'SDCARD_OPENFILE_TOOLTIP': 'If file not exists, it will be created automatically.',
+            'SDCARD_CLOSEFILE': 'sd card close file',
+            'SDCARD_PRINT': 'print %1 %2 to file',
+            'SDCARD_EOL_WARP': 'warp',
+            'SDCARD_EOL_NOWARP': 'no-warp',
+            'SDCARD_FILEDATAAVAILABLE': 'sd card file data available',
+            'SDCARD_READFILEDATA': 'sd card read file data',
+            'SDCARD_ISFILEEXISTS': 'sd card file %1 exists?',
+            'SDCARD_CREATEFILE': 'sd card create file %1',
+            'SDCARD_DELETEFILE': 'sd card delet file %1'
+        },
+        'zh-tw': {
+            'SDCARD_CATEGORY': 'SD Card',
+            'SDCARD_INIT': 'init sd card pin CS %1',
+            'SDCARD_OPENFILE': 'sd card open file %1 mode %2',
+            'SDCARD_MODE_READ': 'read only',
+            'SDCARD_MODE_READWRITE': 'read & write',
+            'SDCARD_OPENFILE_TOOLTIP': 'If file not exists, it will be created automatically.',
+            'SDCARD_CLOSEFILE': 'sd card close file',
+            'SDCARD_PRINT': 'print %1 %2 to file',
+            'SDCARD_EOL_WARP': 'warp',
+            'SDCARD_EOL_NOWARP': 'no-warp',
+            'SDCARD_FILEDATAAVAILABLE': 'sd card file data available',
+            'SDCARD_READFILEDATA': 'sd card read file data',
+            'SDCARD_ISFILEEXISTS': 'sd card file %1 exists?',
+            'SDCARD_CREATEFILE': 'sd card create file %1',
+            'SDCARD_DELETEFILE': 'sd card delet file %1'
+        }
+    }
+};
